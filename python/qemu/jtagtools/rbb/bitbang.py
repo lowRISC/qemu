5636562eeb34db29cfca405f07a018381d826448
Emmanuel Blot <eblot@rivosinc.com>
2024-11-22 19:44:53 +0100

    [ot] python/qemu: bitbang.py: update the new write_tms API from jtagtools.
    Signed-off-by: Emmanuel Blot <eblot@rivosinc.com>

--- python/qemu/jtagtools/rbb/bitbang.py
+++ python/qemu/jtagtools/rbb/bitbang.py
@@ -44,15 +44,15 @@ class JtagBitbangController(JtagController):
     DEFAULT_PORT = 3335
     """Default TCP port."""
 
-    RECV_TIMEOUT = 0.25
+    RECV_TIMEOUT = 2.0
     """Maximum allowed time in seconds to receive a response from the JTAG
        controller.
     """
 
-    READ = b'R'
+    READ = ord('R')
     """JTAG bitbang code to receive data from TDO."""
 
-    QUIT = b'Q'
+    QUIT = ord('Q')
     """JTAG bitbang code to quit."""
 
     def __init__(self, sock: socket, link_log: bool = False):
@@ -61,6 +61,7 @@ class JtagBitbangController(JtagController):
         self._link_log = link_log
         self._last: Optional[bool] = None  # Last deferred TDO bit
         self._outbuf = bytearray()
+        self._inlen = 0
         self._tck = False
         self._tms = False
         self._tdi = False
@@ -76,6 +77,7 @@ class JtagBitbangController(JtagController):
             self._write(self._reset_code(self._trst, self._srst))
         else:
             self.write_tms(BitSequence('11111'))
+        self.scan()
 
     def system_reset(self) -> None:
         self._log.info('System reset')
@@ -84,12 +86,40 @@ class JtagBitbangController(JtagController):
         self._write(self._reset_code(self._trst, self._srst))
         self._srst = not self._srst
         self._write(self._reset_code(self._trst, self._srst))
+        self.scan()
 
     def quit(self) -> None:
         self._log.info('Quit')
-        self._sock.send(self.QUIT)
+        self._outbuf.append(self.QUIT)
+        self.scan()
 
-    def write_tms(self, modesel: BitSequence) -> None:
+    def scan(self) -> BitSequence:
+        if self._outbuf:
+            if self._link_log:
+                self._log.debug('write %d bytes to TDI', len(self._outbuf))
+            self._sock.send(self._outbuf)
+            self._outbuf.clear()
+        bseq = BitSequence()
+        length = self._inlen
+        self._inlen = 0
+        if length:
+            timeout = now() + self.RECV_TIMEOUT
+            while length:
+                try:
+                    data = self._sock.recv(length)
+                except TimeoutError as exc:
+                    if now() < timeout:
+                        continue
+                    raise TimeoutError('No response from RBB server') from exc
+                length -= len(data)
+                bseq.push_right(data)
+                timeout = now() + self.RECV_TIMEOUT / 2
+            bseq.reverse()
+            if self._link_log:
+                self._log.debug('read TDO [%d] %s', len(bseq), bseq)
+        return bseq
+
+    def write_tms(self, modesel: BitSequence, read_tdo: bool = False) -> None:
         if not isinstance(modesel, BitSequence):
             raise ValueError('Expect a BitSequence')
         # apply the last TDO bit
@@ -102,77 +132,64 @@ class JtagBitbangController(JtagController):
             self._log.debug('write TMS [%d] %s', len(modesel), modesel)
         tck = self._tck
         tdi = self._tdi
+        tms = self._tms
+        read = self.READ
         code = self._bus_code
-        stream = bytearray()
         while modesel:
             tms = modesel.pop_left_bit()
-            stream.append(code(tck, tms, tdi))
+            self._outbuf.append(code(tck, tms, tdi))
             tck = not tck
-            stream.append(code(tck, tms, tdi))
+            self._outbuf.append(code(tck, tms, tdi))
             tck = not tck
-        self._sock.send(stream)
+            if read_tdo:
+                self._outbuf.append(read)
+                self._inlen += 1
+                read_tdo = False
         self._tms = tms
         self._tck = tck
 
-    def write(self, out: BitSequence):
+    def write(self, out: BitSequence) -> None:
         if not isinstance(out, BitSequence):
             raise ValueError('out is not a BitSequence')
         if self._last is not None:
             # TODO: check if this case needs to be handled
             raise NotImplementedError('Last is lost')
         self._last = out.pop_left_bit()
-        if self._link_log:
-            self._log.debug('write TDI [%d] %s', len(out), out)
         tms = self._tms
         tck = self._tck
+        tdi = self._tdi
         code = self._bus_code
-        stream = bytearray()
         while out:
             tdi = out.pop_right_bit()
-            stream.append(code(tck, tms, tdi))
+            self._outbuf.append(code(tck, tms, tdi))
             tck = not tck
-            stream.append(code(tck, tms, tdi))
+            self._outbuf.append(code(tck, tms, tdi))
             tck = not tck
-        self._sock.send(stream)
         self._tdi = tdi
         self._tms = tms
         self._tck = tck
 
-    def read(self, length: int) -> BitSequence:
+    def read(self, length: int) -> None:
         if length == 0:
             raise ValueError()
-        bseq = BitSequence()
-        rem = length
-        timeout = now() + self.RECV_TIMEOUT
         if self._link_log:
-            self._log.debug('read %d bits, TMS: %d', length, self._tms)
+            self._log.debug('read %d TDO bits, TMS: %d', length, self._tms)
         tms = self._tms
         tck = self._tck
         tdi = self._tdi
-        read = ord(self.READ)
+        read = self.READ
         code = self._bus_code
-        stream = bytearray()
-        for _ in range(length):
-            stream.append(code(tck, tms, tdi))
+        self._inlen += length
+        while length:
+            length -= 1
+            self._outbuf.append(code(tck, tms, tdi))
             tck = not tck
-            stream.append(code(tck, tms, tdi))
+            self._outbuf.append(code(tck, tms, tdi))
             tck = not tck
-            stream.append(read)
-        self._sock.send(stream)
-        while rem:
-            try:
-                data = self._sock.recv(length)
-            except TimeoutError:
-                if now() < timeout:
-                    continue
-                raise
-            rem -= len(data)
-            bseq.push_right(data)
-            timeout = now() + self.RECV_TIMEOUT
-        bseq.reverse()
-        if self._link_log:
-            self._log.debug('read TDI [%d] %s', len(bseq), bseq)
-        return bseq
+            self._outbuf.append(read)
+
+    def exchange(self, out: BitSequence) -> BitSequence:
+        raise NotImplementedError('RBB exchange support not implemented yet')
 
     @classmethod
     def _bus_code(cls, tclk: bool, tms: bool, tdi: bool) -> int:
@@ -186,4 +203,4 @@ class JtagBitbangController(JtagController):
         if self._link_log:
             self._log.debug('_write 0x%02x %s (%s)', code, f'{code-0x30:03b}',
                             chr(code))
-        self._sock.send(bytes([code]))
+        self._outbuf.append(code)
