ada058d8791406d8baf2ab4fdccee5074b0d0d2d
Alex Jones <alex.jones@lowrisc.org>
2025-08-28 01:09:46 +0100

    [ot] hw/riscv: ot_earlgrey: Add signals between lc_ctrl and otp_ctrl
    Now that Earlgrey's OTP is updated to match Darjeeling and can support
    broadcast signals from the lifecycle controller, hook up the three
    broadcast signals that are relevant on Earlgrey (OWNER_SEED is not
    stored in OTP on EG, unlike DJ, and so there is no need for connections
    with the OTP here). Also connects the escalation signal, though this
    has limited support in the otp_ctrl at present.
    
    The `OT_LC_CREATOR_SEED_SW_RW_EN` also needs to be connected to the OTP.
    Even though it sounds like this should only sound the Software read /
    writability of the Creator Seed (stored in flash on Earlgrey), it
    actually controls any keymgr_creator partition, which includes the
    SECRET2 partition containing the Creator Root Key. So this is maybe more
    aptly a "CREATOR_SECRET_SW_RW_EN" seed, as it pertains to many creator
    secrets and not just the Creator Seed.
    
    Signed-off-by: Alex Jones <alex.jones@lowrisc.org>

--- hw/riscv/ot_earlgrey.c
+++ hw/riscv/ot_earlgrey.c
@@ -690,18 +690,21 @@ static const IbexDeviceDef ot_eg_soc_devices[] = {
              * supporting HW is available:
              *  - OT_LC_NVM_DEBUG_EN (for embed. flash)
              *  - OT_LC_KEYMGR_EN (when keymgr is implemented)
-             *  - OT_LC_CHECK_BYP_EN (when Earlgrey supports OTP signals)
-             *  - OT_LC_CREATOR_SEED_SW_RW_EN (for OTP and embed. flash)
+             *  - OT_LC_CREATOR_SEED_SW_RW_EN (for embed. flash)
              *  - OT_LC_OWNER_SEED_SW_RW_EN (for embed. flash)
              *  - OT_LC_ISO_PART_SW_RD_EN (for embed. flash)
              *  - OT_LC_ISO_PART_SW_WR_EN (for embed. flash)
-             *  - OT_LC_SEED_HW_RD_EN (for OTP and embed. flash)
+             *  - OT_LC_SEED_HW_RD_EN (for embed. flash)
              */
             OT_EG_SOC_D2S(OT_LC_BROADCAST, OT_LC_HW_DEBUG_EN, LC_HW_DEBUG),
             OT_EG_SOC_D2S(OT_LC_BROADCAST, OT_LC_ESCALATE_EN, LC_ESCALATE),
             OT_EG_SOC_D2S(OT_LC_BROADCAST, OT_LC_SEED_HW_RD_EN, LC_SEED_HW_RD),
             OT_EG_SOC_SIGNAL(OT_LC_BROADCAST, OT_LC_CPU_EN, IBEX_WRAPPER,
-                             OT_IBEX_WRAPPER_CPU_EN, OT_IBEX_LC_CTRL_CPU_EN)
+                             OT_IBEX_WRAPPER_CPU_EN, OT_IBEX_LC_CTRL_CPU_EN),
+            OT_EG_SOC_SIGNAL(OT_LC_BROADCAST, OT_LC_CHECK_BYP_EN, OTP_CTRL,
+                             OT_LC_BROADCAST, OT_OTP_LC_CHECK_BYP_EN),
+            OT_EG_SOC_SIGNAL(OT_LC_BROADCAST, OT_LC_CREATOR_SEED_SW_RW_EN, OTP_CTRL,
+                             OT_LC_BROADCAST, OT_OTP_LC_CREATOR_SEED_SW_RW_EN)
         ),
         .link = IBEXDEVICELINKDEFS(
             OT_EG_SOC_DEVLINK("otp_ctrl", OTP_CTRL),
@@ -1332,28 +1335,20 @@ static const IbexDeviceDef ot_eg_soc_devices[] = {
     },
     [OT_EG_SOC_SPLITTER_LC_ESCALATE] = {
         .type = TYPE_SPLIT_IRQ,
-        /*
-         * TODO: Earlgrey OTP signals are not supported yet, add the escalation
-         * connection to the OTP controller when Earlgrey's OTP is updated.
-         * .gpio = IBEXGPIOCONNDEFS(
-         *     OT_EG_SOC_S2D(0, OTP_CTRL, OT_LC_BROADCAST,
-         *                   OT_OTP_LC_ESCALATE_EN)
-         * ),
-         */
+        .gpio = IBEXGPIOCONNDEFS(
+            OT_EG_SOC_S2D(0, OTP_CTRL, OT_LC_BROADCAST,
+                          OT_OTP_LC_ESCALATE_EN)
+        ),
         .prop = IBEXDEVICEPROPDEFS(
             IBEX_DEV_UINT_PROP("num-lines", 1u) // to be changed
         )
     },
     [OT_EG_SOC_SPLITTER_LC_SEED_HW_RD] = {
         .type = TYPE_SPLIT_IRQ,
-        /*
-         * TODO: Earlgrey OTP signals are not supported yet, add the signal
-         * connection to the OTP controller when Earlgrey's OTP is updated.
-         * .gpio = IBEXGPIOCONNDEFS(
-         *   OT_EG_SOC_S2D(0, OTP_CTRL, OT_LC_BROADCAST,
-         *                 OT_OTP_LC_SEED_HW_RD_EN)
-         * ),
-         */
+        .gpio = IBEXGPIOCONNDEFS(
+          OT_EG_SOC_S2D(0, OTP_CTRL, OT_LC_BROADCAST,
+                        OT_OTP_LC_SEED_HW_RD_EN)
+        ),
         .prop = IBEXDEVICEPROPDEFS(
             IBEX_DEV_UINT_PROP("num-lines", 2u)
         )
