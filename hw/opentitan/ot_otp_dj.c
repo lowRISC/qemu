45a77415c403ea402c433ecc7c6232386ceda92d
Alex Jones <alex.jones@lowrisc.org>
2025-08-28 03:00:44 +0100

    [ot] hw/opentitan: ot_otp_dj: Fix IRQ and alert shifts
    For IRQs and alerts, the levels were being masked (but not shifted), and
    then cast to a bool and back to an int. The result in `level` was *then*
    shifted, meaning that value updates for IRQs and alerts after the first
    IRQ and the first alert were being lost, and not actually sending relevant
    signals.
    
    Fix (and simply) the masking/shifting and conversion logic so that IRQs
    and alerts are now handled correctly in the OTP.
    
    Signed-off-by: Alex Jones <alex.jones@lowrisc.org>

--- hw/opentitan/ot_otp_dj.c
+++ hw/opentitan/ot_otp_dj.c
@@ -1095,12 +1095,12 @@ static void ot_otp_dj_update_irqs(OtOTPDjState *s)
     uint32_t levels = s->regs[R_INTR_STATE] & s->regs[R_INTR_ENABLE];
 
     for (unsigned ix = 0; ix < ARRAY_SIZE(s->irqs); ix++) {
-        int level = (int)(bool)(levels & (1u << ix));
+        int level = (int)((levels >> ix) & 0x1u);
         if (level != ibex_irq_get_level(&s->irqs[ix])) {
             trace_ot_otp_update_irq(s->ot_id, ibex_irq_get_level(&s->irqs[ix]),
                                     level);
         }
-        ibex_irq_set(&s->irqs[ix], (int)((level >> ix) & 0x1));
+        ibex_irq_set(&s->irqs[ix], level);
     }
 }
 
@@ -1111,13 +1111,13 @@ static void ot_otp_dj_update_alerts(OtOTPDjState *s)
     levels |= s->alert_bm;
 
     for (unsigned ix = 0; ix < ARRAY_SIZE(s->alerts); ix++) {
-        int level = (int)(bool)(levels & (1u << ix));
+        int level = (int)((levels >> ix) & 0x1u);
         if (level != ibex_irq_get_level(&s->alerts[ix])) {
             trace_ot_otp_update_alert(s->ot_id,
                                       ibex_irq_get_level(&s->alerts[ix]),
                                       level);
         }
-        ibex_irq_set(&s->alerts[ix], (int)((level >> ix) & 0x1u));
+        ibex_irq_set(&s->alerts[ix], level);
     }
 
     /* alert test is transient */
@@ -1126,13 +1126,13 @@ static void ot_otp_dj_update_alerts(OtOTPDjState *s)
 
         levels = s->alert_bm;
         for (unsigned ix = 0; ix < ARRAY_SIZE(s->alerts); ix++) {
-            int level = (int)(bool)(levels & (1u << ix));
+            int level = (int)((levels >> ix) & 0x1u);
             if (level != ibex_irq_get_level(&s->alerts[ix])) {
                 trace_ot_otp_update_alert(s->ot_id,
                                           ibex_irq_get_level(&s->alerts[ix]),
                                           level);
             }
-            ibex_irq_set(&s->alerts[ix], (int)((level >> ix) & 0x1u));
+            ibex_irq_set(&s->alerts[ix], level);
         }
     }
 }
