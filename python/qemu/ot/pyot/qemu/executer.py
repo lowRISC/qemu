lowRISC/qemu

commit db06b4139a6d4c8d8a42d3173b609c1a8504364d

[ot] python/qemu: ot.pyot.executer: use specialized wrapper
QEMU executer should use QEMU wrapper.

Signed-off-by: Emmanuel Blot <eblot@rivosinc.com>

python/qemu/ot/pyot/qemu/executer.py
@@ -17,6 +17,7 @@ from ot.util.file import guess_file_type
 from ot.util.misc import EasyDict
 
 from ..executer import Executer
+from .wrapper import Wrapper
 
 
 class QEMUExecuter(Executer):
@@ -34,6 +35,9 @@ class QEMUExecuter(Executer):
         'U': 'unimp',
     }
     """Shortcut names for QEMU log sources."""
+
+    WRAPPER = Wrapper
+    """QEMU wrapper."""
 
     def _build_fw_args(self, args: Namespace) \
             -> tuple[str, Optional[str], list[str], Optional[str]]:

python/qemu/ot/pyot/executer.py
@@ -83,6 +83,9 @@ class Executer:
     DEFAULT_SERIAL_PORT = 'serial0'
     """Default VCP name."""
 
+    WRAPPER = Wrapper
+    """Default wrapper."""
+
     def __init__(self, tfm: FileManager, config: dict[str, any],
                  args: Namespace):
         self._log = getLogger('pyot.exec')
@@ -131,7 +134,7 @@ class Executer:
            :return: success or the code of the first encountered error
         """
         log_classifiers = self._config.get('logclass', {})
-        qot = Wrapper(log_classifiers, debug)
+        qot = self.WRAPPER(log_classifiers, debug)
         ret = 0
         results = defaultdict(int)
         result_file = self._argdict.get('result')

commit 40c1787e2d8729559699b4d0a6ac4e23b075bc75

[ot] python/qemu: ot.pyot.qemu.executer: fix an issue introduced in db06b413
Signed-off-by: Emmanuel Blot <eblot@rivosinc.com>

python/qemu/ot/pyot/qemu/executer.py
@@ -17,7 +17,7 @@ from ot.util.file import guess_file_type
 from ot.util.misc import EasyDict
 
 from ..executer import Executer
-from .wrapper import Wrapper
+from .wrapper import QEMUWrapper
 
 
 class QEMUExecuter(Executer):
@@ -36,7 +36,7 @@ class QEMUExecuter(Executer):
     }
     """Shortcut names for QEMU log sources."""
 
-    WRAPPER = Wrapper
+    WRAPPER = QEMUWrapper
     """QEMU wrapper."""
 
     def _build_fw_args(self, args: Namespace) \