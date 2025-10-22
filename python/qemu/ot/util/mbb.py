bac75da42f5b93922f72974ecd28cc2b9146a9a9
Emmanuel Blot <eblot@rivosinc.com>
2025-10-22 19:30:18 +0200

    [ot] python/qemu: ot.util.mbb: fix multibit bool values
    Signed-off-by: Emmanuel Blot <eblot@rivosinc.com>

--- python/qemu/ot/util/mbb.py
+++ python/qemu/ot/util/mbb.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024 Rivos, Inc.
+# Copyright (c) 2024-2025 Rivos, Inc.
 # SPDX-License-Identifier: Apache2
 
 """MultiBit Boolean values
@@ -11,5 +11,5 @@ MB4_FALSE = 0x9
 MB4_MASK = 0xf
 
 MB8_TRUE = 0x96
-MB8_FALSE = 0x89
+MB8_FALSE = 0x69
 MB8_MASK = 0xff
