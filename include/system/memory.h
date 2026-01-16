8c720959be5dd58efb1cffc1877a2e616973b702
James Wainwright <james.wainwright@lowrisc.org>
2026-01-16 13:39:28 +0000

    [ot] system: memory: remove unused `mtree_print_as_{flatview,simple}` APIs
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>

--- include/system/memory.h
+++ include/system/memory.h
@@ -2678,9 +2678,6 @@ bool memory_global_dirty_log_start(unsigned int flags, Error **errp);
 void memory_global_dirty_log_stop(unsigned int flags);
 
 void mtree_info(bool flatview, bool dispatch_tree, bool owner, bool disabled);
-void mtree_print_as_flatview(AddressSpace *as, bool dispatch_tree, bool owner);
-void mtree_print_as_simple(AddressSpace *as, bool dispatch_tree, bool owner,
-                           bool disabled);
 
 bool memory_region_access_valid(MemoryRegion *mr, hwaddr addr,
                                 unsigned size, bool is_write,
